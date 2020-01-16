fbcfbe9e5eeed5261d667738eeedacfc0def2586
Stanislav Urasov <erloir@gmail.com>
2020-01-16 12:32:06 +0200

    1st task
    Added scripts for main browsers

---
+++ Scripts/Task 1 Different browsers/IE.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+
+driver = webdriver.Ie()
+driver.get("https://google.com")
+assert "Google" in driver.title
+driver.find_element_by_name("q").click()
+driver.find_element_by_name("q").send_keys("Apriorit")
+driver.find_element_by_name("q").submit()
+print(driver.title)
+assert "Apriorit - Пошук Google" in driver.title
+
+
+
+driver.close()
